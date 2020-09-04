a9d17a182768031912fc0d72adb6816aead9a634
Ryan Rotman <ryan.rotman@gmail.com>
2020-09-03 20:47:24 -0500

    copied working code from my branch of group project

---
+++ script.js
@@ -0,0 +1,52 @@
+// This is the API key I set up through Google
+var apiKey = "AIzaSyBXaAKr4axxaUBPZXJD-cKQF9qtHVrzXe0";
+
+/* Toggle modal on by adding class is-active
+TODO:
+    - would just have to add this wherever in the code makes sense for it to pop up
+    - and by what method, ie. a button
+*/
+$(".modal").addClass("is-active");
+
+// Select the user input for each field on the modal
+$("#submit-button").on("click", function() {
+    event.preventDefault();
+    var zipOnly = $("#zip-only").val();
+    if (zipOnly) {
+        console.log("ZIP Only: " + zipOnly);
+        // Build the query URL
+        // Search parameters start after the q= and use either + or %20 to escape spaces
+        var queryURL = `https://www.google.com/maps/embed/v1/search?key=${apiKey}&q=bars+near+${zipOnly}`;
+        console.log("QueryURL: " + queryURL);
+    } else {
+        var address = $("#address").val().trim();
+        address = address.replace(/ /g, "+");
+        console.log("Address: " + address);
+        var city = $("#city").val().trim();
+        city = city.replace(/ /g, "+");
+        console.log("City: " + city);
+        var state = $("#state option:selected").val();
+        console.log("State: " + state);
+        var zip = $("#zip").val().trim();
+        console.log("Zip: " + zip);
+        // Build the query URL
+        // Search parameters start after the q= and use either + or %20 to escape spaces
+        var queryURL = `https://www.google.com/maps/embed/v1/search?key=${apiKey}&q=bars+near+${address},${city},${state}+${zip}`;
+        console.log("QueryURL: " + queryURL);
+    };
+    $(".modal").removeClass("is-active");
+    var embedMap = $("#embed-map");
+    embedMap.attr("src", queryURL);
+});
+
+/*
+FIXME:
+    - how do we work around if a user DOES NOT want to search for bars?
+        - will need to write code that it will stop the function if no input is listed
+        - or add another button in the modal that simply just closes the modal (.removeClass("is-active"))
+*/
+
+// TEST: Close modal using X in the corner (this is one option for fixing the above fixme)
+$(".modal-close").on("click", function() {
+    $(".modal").removeClass("is-active");
+});
